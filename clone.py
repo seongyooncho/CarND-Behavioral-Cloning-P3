d3b80a7e1a9e2f0de2d13e752bffa652f7cb0de3
Seongyoon Cho <seongyoon.cho@ixdl.kr>
2017-05-24 09:10:47 +0000

    Crop images

--- clone.py
+++ clone.py
@@ -19,20 +19,39 @@ for line in lines:
   measurement = float(line[3])
   measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images = []
+augmented_measurements = []
+for image, measurement in zip(images, measurements):
+  augmented_images.append(image)
+  augmented_measurements.append(measurement)
+  augmented_images.append(np.fliplr(image))
+  augmented_measurements.append(-measurement)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 print(X_train.shape)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((75, 20), (0, 0))))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
 model.save('model.h5')
 
+import gc; gc.collect()
