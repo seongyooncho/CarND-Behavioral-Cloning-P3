89bb53ece5777e201306e1597b3edcbaec744d7f
Jaeyoung Lee <jylee0121@gmail.com>
2017-05-24 07:24:12 +0000

    Bug fix

--- clone.py
+++ clone.py
@@ -8,12 +8,12 @@ with open('./data/driving_log.csv') as csvfile:
   for line in reader:
       lines.append(line)
 
-images = [] 
+images = []
 measurements = []
 for line in lines:
   source_path = line[0]
   filename = source_path.split('/')[-1]
-  current_path = './data/IMG' + filename
+  current_path = './data/IMG/' + filename
   image = cv2.imread(current_path)
   images.append(image)
   measurement = float(line[3])
@@ -21,6 +21,8 @@ for line in lines:
 
 X_train = np.array(images)
 y_train = np.array(measurements)
+
+print(X_train.shape)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
@@ -30,7 +32,7 @@ model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
 
 model.save('model.h5')
 
