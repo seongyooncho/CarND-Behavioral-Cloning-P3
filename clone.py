0b796362e9058110eb7f5eea763c5897d9d240c0
Jaeyoung Lee <jylee0121@gmail.com>
2017-05-25 03:06:16 +0000

    Refactor using generator

--- clone.py
+++ clone.py
@@ -2,36 +2,48 @@ import csv
 import cv2
 import numpy as np
 import gc
+from sklearn.utils import shuffle
+from sklear.model_selection import train_test_split
 
-lines = []
+samples = []
 with open('./data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
-      lines.append(line)
+    line.append(1) #flag for augmentation
+    samples.append(line)
 
-images = []
-measurements = []
-for line in lines:
-  source_path = line[0]
-  filename = source_path.split('/')[-1]
-  current_path = './data/IMG/' + filename
-  image = cv2.imread(current_path)
-  images.append(image)
-  measurement = float(line[3])
-  measurements.append(measurement)
+def generator(samples, batch_size=32):
+  num_samples = len(samples)
+  while 1:
+    shuffle(samples)
+    for offset in range(0, num_samples, batch_size):
+      batch_samples = samples[offset:offset+batch_size]
 
-augmented_images = []
-augmented_measurements = []
-for image, measurement in zip(images, measurements):
-  augmented_images.append(image)
-  augmented_measurements.append(measurement)
-  augmented_images.append(np.fliplr(image))
-  augmented_measurements.append(-measurement)
+      images = []
+      angles = []
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+      for batch_sample in batch_samples:
+        name = './data/IMG/' + batch_sample[0].split('/')[-1]
+        center_image = cv2.imread(name)
+        center_angle = float(batch_sample[3])
+        images.append(center_image)
+        angles.append(center_angle)
 
-print(X_train.shape)
+      X_train = np.array(images)
+      y_train = np.array(angles)
+      yield shuffle(X_train, y_train)
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
+
+#augmented_images = []
+#augmented_measurements = []
+#for image, measurement in zip(images, measurements):
+#  augmented_images.append(image)
+#  augmented_measurements.append(measurement)
+#  augmented_images.append(np.fliplr(image))
+#  augmented_measurements.append(-measurement)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
@@ -51,7 +63,13 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10, verbose=1)
+#history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10, verbose=1)
+history_object = model.fit_generator(
+        train_generator, 
+        len(train_samples), 
+        validation_data=validation_generator, 
+        validation_steps=len(validation_samples),
+        epochs=3)
 
 model.save('model.h5')
 
