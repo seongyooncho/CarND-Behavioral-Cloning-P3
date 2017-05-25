8b92f516ed541b928d82a6e6104bc923560ebe2c
Jaeyoung Lee <jylee0121@gmail.com>
2017-05-25 09:08:12 +0000

    Bug fix for fit_generator

--- model.py
+++ model.py
@@ -3,13 +3,17 @@ import cv2
 import numpy as np
 import gc
 from sklearn.utils import shuffle
-from sklear.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+
+BATCH_SIZE = 32
 
 samples = []
 with open('./data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
-    line.append(1) #flag for augmentation
+    line.append(1)
+    samples.append(line)
+    line[-1] = -1
     samples.append(line)
 
 def generator(samples, batch_size=32):
@@ -23,9 +27,12 @@ def generator(samples, batch_size=32):
       angles = []
 
       for batch_sample in batch_samples:
-        name = './data/IMG/' + batch_sample[0].split('/')[-1]
+        name = './data/IMG/'+batch_sample[0].split('/')[-1]
         center_image = cv2.imread(name)
         center_angle = float(batch_sample[3])
+        if (batch_sample[-1] == -1):
+          center_image = np.fliplr(center_image)
+          center_angle = - center_angle
         images.append(center_image)
         angles.append(center_angle)
 
@@ -34,28 +41,20 @@ def generator(samples, batch_size=32):
       yield shuffle(X_train, y_train)
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-train_generator = generator(train_samples)
-validation_generator = generator(validation_samples)
-
-#augmented_images = []
-#augmented_measurements = []
-#for image, measurement in zip(images, measurements):
-#  augmented_images.append(image)
-#  augmented_measurements.append(measurement)
-#  augmented_images.append(np.fliplr(image))
-#  augmented_measurements.append(-measurement)
+train_generator = generator(train_samples, batch_size = BATCH_SIZE)
+validation_generator = generator(validation_samples, batch_size = BATCH_SIZE)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((75, 20), (0, 0))))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(Conv2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(Conv2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
@@ -63,19 +62,16 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-#history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10, verbose=1)
 history_object = model.fit_generator(
         train_generator, 
-        len(train_samples), 
-        validation_data=validation_generator, 
-        validation_steps=len(validation_samples),
-        epochs=3)
+        steps_per_epoch = len(train_samples) // BATCH_SIZE + 1, 
+        validation_data = validation_generator, 
+        validation_steps = len(validation_samples) // BATCH_SIZE + 1,
+        epochs=10)
 
 model.save('model.h5')
 
 gc.collect()
-
-print(history_object.history.keys())
 
 import matplotlib as mpl
 mpl.use('Agg')
