6557c0a544beeac0f8463c38174b76e45dd9be3f
Jaeyoung Lee <jylee0121@gmail.com>
2017-05-25 01:37:23 +0000

    Add visualization

--- clone.py
+++ clone.py
@@ -1,6 +1,7 @@
 import csv
 import cv2
 import numpy as np
+import gc
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
@@ -50,8 +51,22 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10, verbose=1)
 
 model.save('model.h5')
 
-import gc; gc.collect()
+gc.collect()
+
+print(history_object.history.keys())
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('history.png')
